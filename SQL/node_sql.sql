1b1b1fbecaa7385456917ae536c4196152a27a99
相顾无相识 <598443365@qq.com>
2024-03-21 19:31:14 +0800

    参考图更新

---
+++ SQL/node_sql.sql
@@ -0,0 +1,26 @@
+﻿# Host: localhost  (Version: 5.7.26)
+# Date: 2023-05-29 12:37:10
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `username` varchar(25) NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` varchar(18) NOT NULL DEFAULT '' COMMENT '用户密码',
+  `name` varchar(15) DEFAULT NULL COMMENT '用户昵称',
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='用户信息';
+
+#
+# Data for table "users"
+#
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','123456','管理员');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
--- covid-19_dvp/src/main.ts
+++ covid-19_dvp/src/main.ts
@@ -4,9 +4,9 @@ import router from './router';
 import store from './store';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ElementPlus from 'element-plus'
-import './../public/CSS/adaptive.css'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import './../public/CSS/adaptive.css'
 
 const app = createApp(App)
 app.use(ElementPlus, {
